bb2aa272f19eeedce1de14be08f3ac1c0b670f20
PC <PC@DESKTOP-MCPU5FH>
2021-03-28 22:14:11 +0800

    first version

---
+++ InfraStructure/StockData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraStructure
+{
+    public class StockData
+    {
+        public StockData(string sData)
+        {
+
+        }
+        public DateTime Date { get; set; }
+        public double OpenPrice { get; set; }
+        public double ClosePrice { get; set; }
+        public double HightestPrice { get; set; }
+        public double LowestPrice { get; set; }
+        public double Volumn { get; set; }
+
+        public static explicit operator StockData(string sData) => new StockData(sData);
+    }
+}
---
+++ StockPredictCore/DataParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfraStructure;
+
+namespace StockPredictCore
+{
+    public class DataParser
+    {
+        public List<StockData> GrabData(string filepath)
+        {
+            List<StockData> result = new List<StockData>();
+
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                result.Add((StockData)lines[i]);
+            }
+             
+            return result;
+        }
+    }
+}
