99612f94aaa03b7a51a6f488d812b6501756cdc5
PC <PC@DESKTOP-MCPU5FH>
2021-07-19 22:49:06 +0800

    新增演算法策略

---
+++ StockPredictor/Class/AlgoStategy/AlgoStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace StockPredictor.Class
+{
+    public class AlgoStrategy : ObservableObject
+    {
+        private string name = "演算法策略";
+        public string Name
+        {
+            get => name;
+            set { Set(() => Name, ref name, value); }
+        }
+
+        private ObservableCollection<FilterInfo.FilterInfo> filterInfoList = new ObservableCollection<FilterInfo.FilterInfo>();
+
+        public ObservableCollection<FilterInfo.FilterInfo> FilterInfoList
+        {
+            get => filterInfoList;
+            set { Set(() => FilterInfoList, ref filterInfoList, value); }
+        }
+
+        public AlgoStrategy()
+        {
+
+        }
+    }
+}
---
+++ StockPredictor/Class/AlgoStategy/AlgoStrategyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace StockPredictor.Class.AlgoStategy
+{
+    public class AlgoStrategyService
+    {
+        public void Save(List<AlgoStrategy> algoStrategy)
+        { 
+            var json = new JavaScriptSerializer().Serialize(algoStrategy);
+        }
+
+        public List<AlgoStrategy> Load(string file)
+        {
+            var data = new JavaScriptSerializer().DeserializeObject(file);
+
+            return data as List<AlgoStrategy>;
+        }
+    }
+}
