b37b7f1a597c3de496fb01d7044e808a4f52d4f2
PC <PC@DESKTOP-MCPU5FH>
2022-01-11 23:32:11 +0800

    K

--- StockPredictor/ViewModel/StockDetailViewModel.cs
+++ StockPredictor/ViewModel/StockDetailViewModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using GalaSoft.MvvmLight;
 using InfraStructure;
+using StockPredictCore.Filter.AverageLine;
 
 namespace StockPredictor.ViewModel
 {
@@ -27,7 +29,7 @@ namespace StockPredictor.ViewModel
         }
 
         //Ma5與Ma60交換發生時間點
-        private List<FeatureInfo> ma5ma60MappingFeatureList = new List<FeatureInfo>();
+        private List<FeatureInfo> ma5ma60MappingFeatureList;
 
         public List<FeatureInfo> Ma5ma60MappingFeatureList
         {
@@ -39,16 +41,59 @@ namespace StockPredictor.ViewModel
         {
             StockID = stockData.ID;
             StockName = stockData.Name;
-
-
-
+             
+            Ma5ma60MappingFeatureList = GetMa5ma60MappingFeatureListData(stockData);
         }
 
         private List<FeatureInfo> GetMa5ma60MappingFeatureListData(StockData source)
         {
+            for (int i = 0; i < source.IsFilter.Length; i++)
+            {
+                source.IsFilter[i] = false;
+            }
             List<FeatureInfo> result = new List<FeatureInfo>();
+            List<StockData> data = new List<StockData>(){ source };
+            Ma5OverMa60Filter ma5OverMa60Filter = new Ma5OverMa60Filter(data, 0);
+            Ma60OverMa5Filter ma60OverMa5Filter = new Ma60OverMa5Filter(data, 0);
 
+            ma5OverMa60Filter.Execute();
+            
+            source = data.First();
+            for (int i = 0; i < source.Date.Length; i++)
+            {
+                if(source.IsFilter[i])
+                    continue;
 
+                FeatureInfo feature = new FeatureInfo()
+                {
+                    Date = source.Date[i],
+                    ClosedPrice = source.ClosePrice[i],
+                    Status = "Ma5穿過Ma60"
+                };
+                result.Add(feature);
+            }
+
+            for (int i = 0; i < source.IsFilter.Length; i++)
+            {
+                source.IsFilter[i] = false;
+            }
+            ma60OverMa5Filter.Execute();
+
+            for (int i = 0; i < source.Date.Length; i++)
+            {
+                if (source.IsFilter[i])
+                    continue;
+
+                FeatureInfo feature = new FeatureInfo()
+                {
+                    Date = source.Date[i],
+                    ClosedPrice = source.ClosePrice[i],
+                    Status = "Ma60穿過Ma5"
+                };
+                result.Add(feature);
+            }
+
+            result = result.OrderByDescending(_ => _.Date).ToList();
 
             return result;
         }
