eeed21249659e229ebfc30617a118c9ba4dc3fa3
xilian <xilian@pixl.world>
2023-01-24 14:29:14 +0800

    add calculateService calMA

---
+++ StockPredictCore.Test/ServiceTest/CalculateServiceTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using InfraStructure;
+using StockPredictCore.Service;
+
+namespace StockPredictCore.Test.ServiceTest
+{
+    /// <summary>
+    /// Summary description for CalculateServiceTest
+    /// </summary>
+    [TestClass]
+    public class CalculateServiceTest
+    {
+        private readonly CalculateService calculateService = new CalculateService();
+        public CalculateServiceTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+       
+        [TestMethod]
+        public void Test_MA_0to5()
+        {
+            double[] data = new double[5]{1,2,3,4,5};
+            int avgDays = 5;
+            var result = calculateService.Cal_MA(data,avgDays);
+
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(0, result[1]);
+            Assert.AreEqual(0, result[2]);
+            Assert.AreEqual(0, result[3]);
+            Assert.AreEqual(3, result[4]);
+
+
+        }
+
+        [TestMethod]
+        public void Test_MA_0to100()
+        {
+
+            int avgDays = 10;
+            double[] data = new double[100];
+
+            for (int i = 0;i < data.Length; i++)
+            {
+                data[i] = i;
+            }
+
+            var result = calculateService.Cal_MA(data, avgDays);
+
+            Assert.AreEqual(5.5, result[10]);
+
+        }
+    }
+}
---
+++ StockPredictCore/Service/CalculateService.cs
@@ -0,0 +1,43 @@
+﻿using InfraStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockPredictCore.Service
+{
+    public interface ICalculateService
+    {
+        double[] Cal_MA(double[] data, int avgDays);
+    }
+    public class CalculateService : ICalculateService
+    {
+        public double[] Cal_MA(double[] data, int avgDays)
+        {
+            if (data == null)
+                return Array.Empty<double>();
+
+            double[] result = new double[data.Length];
+
+
+            int first = 0, last = 0;
+
+            double sumresult = 0;
+            while (last < data.Length)
+            {
+                sumresult += data[last];
+
+                
+                if (last - first >= avgDays - 1)
+                {
+                    result[last] = sumresult / avgDays;
+                    sumresult -= data[first];
+                    first++;
+                }
+                last++;
+            }
+            return result;
+        }
+    }
+}
